e9d713d5d9c427d4a59a4f8c00aca7307c16a857
Saurabh Niwate <niwatesaurabh2000@gmail.com>
2024-10-27 23:41:35 +0530

    Frontend folder structure created

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,15 @@
 
 import React from 'react'
+import { Outlet } from 'react-router-dom'
+import Navbar from '../src/components/Navbar/Navbar.jsx';
+import Footer from '../src/components/Footer/Footer.jsx';
 
 const App = () => {
   return (
     <div>
-      
+        <Navbar />
+        <Outlet />
+        <Footer />
     </div>
   )
 }
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,9 +2,39 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Body from './pages/Body/Body.jsx';
+import Login from './pages/Login/Login.jsx';
+import NotFound from './components/NotFound/NotFound.jsx';
+
+const router = createBrowserRouter([
+      {
+        path: '/',
+        element: <App />,
+        children: [
+          {
+            index: true,
+            element: <Body />
+          },
+          {
+            path: '/home',
+            element: <Body />
+          }
+        ]
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "*",
+        element: <NotFound />,
+      },
+]);
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <RouterProvider router={router} />
 )
