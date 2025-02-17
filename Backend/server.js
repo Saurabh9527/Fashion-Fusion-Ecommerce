9307c79ac87165be8281f268c22484ebbd819046
Saurabh Niwate <niwatesaurabh2000@gmail.com>
2025-02-17 12:20:12 +0530

    Backend url changed

--- Backend/server.js
+++ Backend/server.js
@@ -21,8 +21,7 @@ const app = express();
 
 const corsOptions = {
   //origin: "*",
-  // origin: "http://localhost:5173", 
-  origin: "https://fashion-fusion-ecommerce-project.vercel.app", 
+  origin: "http://localhost:5173", 
   methods: ["GET", "POST", "HEAD", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
