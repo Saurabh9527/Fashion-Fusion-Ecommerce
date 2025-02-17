6c3b8525c939a1eb5bb462bbce266421423d2782
Saurabh Niwate <niwatesaurabh2000@gmail.com>
2025-02-17 12:16:04 +0530

    Project deployed

--- Backend/server.js
+++ Backend/server.js
@@ -21,7 +21,8 @@ const app = express();
 
 const corsOptions = {
   //origin: "*",
-  origin: "http://localhost:5173", 
+  // origin: "http://localhost:5173", 
+  origin: "https://fashion-fusion-ecommerce-project.vercel.app/", 
   methods: ["GET", "POST", "HEAD", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
